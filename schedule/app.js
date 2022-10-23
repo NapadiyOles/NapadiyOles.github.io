cb022799b8b85ca81df5bac317efe3157af35886
GhostDragon <44135388+NapadiyOles@users.noreply.github.com>
2022-10-23 23:23:15 +0300

    fixed bug with day counter

--- schedule/app.js
+++ schedule/app.js
@@ -3,9 +3,9 @@ function f()
     let currentDate = new Date();
     let startDate = new Date(currentDate.getFullYear(), 0, 1);
     let days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
-    let weekNumber = Math.ceil(days / 7);
+    let weekNumber = Math.ceil((days - 1) / 7);
     let table, label
-    
+
     if(weekNumber % 2 === 0){
         table = document.getElementById('fw')
         label = document.getElementById('fw-label')
@@ -22,12 +22,19 @@ function f()
     const rlen = rows.length
     const day = new Date().getDay()
 
-    if([6, 7].includes(day)){
+    if([6, 0].includes(day)){
+        let text = label.innerHTML;
+        label.onmouseover = function() {
+            this.innerHTML = 'Сьогодні вихідний ✌'
+        }
+        label.onmouseleave = function (){
+            this.innerHTML = text
+        }
         return
     }
 
     for (let i = 0; i < rlen; i++){
-    let cells = rows[i].getElementsByTagName('td')
+        let cells = rows[i].getElementsByTagName('td')
         cells[day].style.background = 'lightblue'
     }
 }
